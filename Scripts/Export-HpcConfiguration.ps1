b7930d2161136eae5a563856ac1d382f6ca40caa
Sunbin Zhu <suzhu@microsoft.com>
2023-06-16 12:39:13 +0800

    add configuration script
    add configuration script

---
+++ Scripts/Export-HpcConfiguration.ps1
@@ -0,0 +1,245 @@
+﻿<#
+
+.SYNOPSIS
+Export Windows HPC Cluster Configuration Settings.
+
+.DESCRIPTION
+This script allows a user to export Windows HPC cluster configuration settings.
+
+.PARAMETER Path
+The export destination path.
+
+.PARAMETER Configuration
+The name of the configuration settings to export. Valid values: "Diagnostics","Management","Reporting","Scheduler","Soa" or none for all configurations.
+
+.PARAMETER DontAppendTimeStampFolder
+The flag to turn off creating a sub folder in the given path.
+
+.EXAMPLE
+Export-HpcConfiguration.ps1
+
+.EXAMPLE
+Export-HpcConfiguration.ps1 -Path:c:\export -Configuration:Scheduler
+
+.EXAMPLE
+Export-HpcConfiguration.ps1 -Path:c:\export -DontAppendTimeStampFolder
+
+.EXAMPLE
+Export-HpcConfiguration.ps1 -Path:c:\export -Configuration:Scheduler,Soa
+
+.NOTES
+
+.LINK
+
+#>
+Param(
+    [switch] $DontAppendTimeStampFolder,
+
+    # The export destination path.
+    [ValidateNotNullOrEmpty()]
+    [string] $Path, 
+
+    # The names of the configuration settings to export. Valid values: "Diagnostics","Management","Reporting","Scheduler","Soa" or none for all configurations.
+    [ValidateSet("Diagnostics","Management","Reporting","Scheduler","Soa")]
+    [string[]] $Configuration
+)
+
+	function PreExportItem( [string] $Name, [string] $Target, [bool] $IsFolder) {
+		WriteOutput ("Exporting $Name to $Target.");
+		$Global:OldErrorCount = $Global:Error.Count;
+		$Global:WarningForCmdlet = $null;
+		$Global:Xml += '<ExportedConfiguration Name="' + $Name + '" Target="' + $Target + '" />';
+		if ( !$IsFolder ) { $Target = [System.IO.Path]::GetDirectoryName($Target); }
+		if( !(Test-Path $Target) ) { New-Item -Path $Target -ItemType directory | out-null; }
+	}
+	
+	function PostExportItem {
+		if( $Global:Error.Count -gt $Global:OldErrorCount ) { $Global:FailureCount++; } else { $Global:SuccessCount++; }
+		$Global:WarningForCmdlet | ? { $_ -ne $null } | % {
+			$Global:Warning += "WARNING: " + $_.Message + [environment]::NewLine + [environment]::NewLine;
+			$Global:Verbose += "WARNING: " + $_.Message + [environment]::NewLine + [environment]::NewLine;
+		}
+	}
+	
+	function WriteWarning( [string] $warning) {
+		Write-Warning $warning;
+		$Global:Warning += "WARNING: $warning" + [environment]::NewLine + [environment]::NewLine;
+		$Global:Verbose += "WARNING: $warning" + [environment]::NewLine + [environment]::NewLine;
+	}
+	
+	function WriteOutput( [string] $output) {
+		Write-Output $output;
+		$Global:Verbose += $output + [environment]::NewLine + [environment]::NewLine;
+	}
+
+	$error.Clear();
+
+	$errorMsgNonElevatedProcess = "This function needs to be run with elevated PowerShell.";
+	$currentPrincipal = New-Object Security.Principal.WindowsPrincipal( [Security.Principal.WindowsIdentity]::GetCurrent() );
+	if(!$currentPrincipal.IsInRole( [Security.Principal.WindowsBuiltInRole]::Administrator )) { Write-Error $errorMsgNonElevatedProcess; return; }
+
+	$errorMsgCannotOnlyRunOnHeadNode = "This function needs to be run on head node.";
+	if( !(Test-Path HKLM:\Software\Microsoft\HPC) -or (Get-ItemProperty -Path:HKLM:\Software\Microsoft\HPC).InstalledRole -inotcontains "HN") { Write-Error $errorMsgCannotOnlyRunOnHeadNode; return; }
+
+	if( !(Get-PsSnapIn Microsoft.HPC -EA:0) ) { Add-PsSnapIn Microsoft.HPC; }
+	$error.Clear();
+
+	if ([string]::IsNullOrEmpty($path)) {
+		if ($DontAppendTimeStampFolder) {
+			# The script will surely fail
+			$errorMsgNeedPathParam = "-DontAppendTimeStampFolder switch is specified, while -Path parameter is missing. Please include the path information in the parameter list. The folder indicated by the path will be created by this script.";
+			Write-Error $errorMsgNeedPathParam;
+			return;
+		}
+        
+		# Assign path as current folder.
+		$Path = ".";
+	}
+       
+	if (!$DontAppendTimeStampFolder) {
+		$Path = [System.IO.Path]::Combine($Path, [System.DateTime]::Now.ToString("yyyyMMdd_HHmmss"));
+	}
+       
+	$errorMsgCannotCreateTargetFolder = "Cannot create exporting target folder ""$Path"". The directory already exists or the given path is invalid.";
+    
+	# We only work with path which does not exist. This prevents data over-writing handling
+	if ([System.IO.Directory]::Exists($Path)){
+		Write-Error $errorMsgCannotCreateTargetFolder;
+		return;
+	}
+    
+	[System.IO.Directory]::CreateDirectory($path) | out-null;
+    
+	# We need to ensure the created path is a disk IO object and the CreateDirectory function completes successfully
+	# CreateDirectory won't create folder in registry
+	# CreateDirectory may fail for invalid path or bad permission.    
+	if( (Get-ItemProperty $Path) -eq $null -or (Get-ItemProperty $Path).GetType().Name -ne "DirectoryInfo") {
+		Write-Error $errorMsgCannotCreateTargetFolder;
+		return;
+	}
+
+	$Path = [System.IO.Path]::GetFullPath($Path);
+
+	WriteOutput("Setting exporting target folder to ""$Path"".");
+
+	if($Configuration -eq $null) { $Configuration = "Diagnostics","Management","Reporting","Scheduler","Soa"; }
+	$version = (Get-HpcClusterOverview).Version;
+
+	if( $error ) { return; }
+
+	$Global:SuccessCount = $Global:FailureCount = 0;
+	$Global:Warning = $Global:Verbose = "";
+	$Global:Xml = '<?xml version="1.0" encoding="utf-8"?><!-- Note:Changing ExportedConfiguration element in this file will not impact the behavior of Import-HpcConfiguration later. --><ExportedConfigurations HpcVersion="' + $version + '" ClusterName="' + $Env:CCP_SCHEDULER + '" Time="' + [System.DateTime]::Now.ToString() + '" CmdLine="' + $MyInvocation.Line + '" Operator="' + $Env:USERDOMAIN + '\' + $Env:USERNAME + '">';
+
+	$redirectToExportFolderCfg = "ActivationFilterProgram", "SubmissionFilterProgram";
+	$bypassCfg = "ActivationFilterProgram","AdFailurePercentageEntry","BackfillLoadPeriod","DisableCredentialReuse","EmailCredential","EventListenerClosePercentage","InstallCredential","RestoreMode","SchedulerOnAzure","SchedulerWebServicePort","SchedulerWebServiceEnabled","SchedulerWebServiceThumbprint","SchedulerWebServiceAuth","SpoolDir","SqlReadFailurePercentageEntry","SqlTransactionFailurePercentage","SubmissionFilterProgram","ReportingDbSize";
+	$bypassSchdEnvVar = "CCP_CLUSTER_NAME","CCP_MPI_NETMASK","CCP_SERVICEREGISTRATION_PATH","WCF_NETWORKPREFIX";
+	$diagCfgNames = "TtlCompletedRuns","RunCleanUpHour","ConcurrencyTestRunNumber";
+	$mgmtCfgNames = "AzureCollectionInterval","CollectCounters","MinuteCounterRetention","NodeNamingSeries","WDSMode","InstallCredential","AzureStorageConnectionString", "AzureLogsToBlob", "AzureLogsToBlobThrottling", "AzureLogsToBlobInterval", "AzureMetricsCollectionEnabled","AzureMetricsJobStatisticsDelayMinutes","AzureIaaSMetricsCollectionEnabled","OperationArchive","OperationRetention";
+	$rptCfgNames = "DataExtensibilityEnabled","DataExtensibilityTtl","AllocationHistoryTtl","ReportingDbSize";
+
+    PreExportItem -Name:"Cluster registry" -Target:"$Path\ClusterRegistry.xml" -IsFolder:$false;
+    Get-HpcClusterRegistry | Export-CliXml -Path "$Path\ClusterRegistry.xml";
+    PostExportItem;
+
+	if($Configuration -icontains "Diagnostics") {
+		PreExportItem -Name:"diagnostics configurations" -Target:"$Path\Diagnostics" -IsFolder:$true;
+		Get-HpcClusterProperty -Parameter -WA:SilentlyContinue | ? {$diagCfgNames -icontains $_.Name -and $bypassCfg -inotcontains $_.Name} | Export-CliXml -Path "$Path\Diagnostics\DiagnosticsCfg.xml";
+		PostExportItem;
+	}
+	
+	if($Configuration -icontains "Management") {
+        $installShare = (Get-HpcClusterRegistry -PropertyName InstallShare).Value
+		PreExportItem -Name:"drivers" -Target:"$Path\Management\HpcDriver" -IsFolder:$true;
+		if( Test-Path "$installShare\Drivers" ) { Copy-Item "$installShare\Drivers\*" "$Path\Management\HpcDriver" -Recurse | out-null; }
+		PostExportItem;
+
+		PreExportItem -Name:"images" -Target:"$Path\Management\HpcImage" -IsFolder:$true;
+		if ( Test-Path "$installShare\Images") { Copy-Item "$installShare\Images\*" "$Path\Management\HpcImage" -Recurse | out-null; }
+		PostExportItem;
+
+		PreExportItem -Name:"users" -Target:"$Path\Management\HpcUser.xml" -IsFolder:$false;
+		Get-HpcMember -WV:+Global:WarningForCmdlet | ? { !($_.Name.EndsWith("$")) } | Export-Clixml -Path "$Path\Management\HpcUser.xml";
+		WriteWarning "Machine accounts are not exported as HPC users for security reason.";
+		PostExportItem;
+
+		PreExportItem -Name:"node groups" -Target:"$Path\Management\HpcNodeGroup.xml" -IsFolder:$false;
+		Get-HpcGroup -WV:+Global:WarningForCmdlet | Export-Clixml -Path "$Path\Management\HpcNodeGroup.xml";
+		PostExportItem;
+
+		PreExportItem -Name:"node templates" -Target:"$Path\Management\HpcNodeTemplate" -IsFolder:$true;
+		Get-HpcNodeTemplate -WV:+Global:WarningForCmdlet | % { if( $_.Name -ne "HeadNode Template" –and $_.Name –ne "LinuxNode Template" –and $_.Name –ne "NonDomain ComputeNode Template" -and $_.Name -ne "Default ComputeNode Template") { Export-HpcNodeTemplate -Template $_ -Path "$Path\Management\HpcNodeTemplate\$($_.Name).xml" -WV:+Global:WarningForCmdlet; } };
+		PostExportItem;
+
+		PreExportItem -Name:"management configurations" -Target:"$Path\Management\ManagementCfg.xml" -IsFolder:$false;
+		Get-HpcClusterProperty -WV:+Global:WarningForCmdlet | ? {$mgmtCfgNames -icontains $_.Name -and $bypassCfg -inotcontains $_.Name} | Export-CliXml -Path "$Path\Management\ManagementCfg.xml";
+		PostExportItem;
+
+		PreExportItem -Name:"hpc metrics" -Target:"$Path\Management\Metrics.xml" -IsFolder:$false;
+		Export-HpcMetric -Path "$Path\Management\Metrics.xml";
+		PostExportItem;
+
+		PreExportItem -Name:"hpc nodes" -Target:"$Path\Management\HpcNode.xml" -IsFolder:$false;
+		Export-HpcNodeXml -Path "$Path\Management\HpcNode.xml";
+		PostExportItem;
+	}
+
+	if($Configuration -icontains "Reporting") {
+		PreExportItem -Name:"reporting configurations" -Target:"$Path\Reporting" -IsFolder:$true;
+		Get-HpcClusterProperty -Parameter -WA:SilentlyContinue | ? {$rptCfgNames -icontains $_.Name -and $bypassCfg -inotcontains $_.Name} | Export-CliXml -Path "$Path\Reporting\ReportingCfg.xml";
+		PostExportItem;
+	}
+
+	if($Configuration -icontains "Scheduler") {
+		PreExportItem -Name:"job templates" -Target:"$Path\Scheduler\HpcJobTemplate" -IsFolder:$true;
+		Get-HpcJobTemplate -WV:+Global:WarningForCmdlet | % { Export-HpcJobTemplate -Template $_ -Path "$Path\Scheduler\HpcJobTemplate\$($_.Name).xml" -WV:+Global:WarningForCmdlet; }
+		PostExportItem;
+
+		PreExportItem -Name:"job template ACL" -Target:"$Path\Scheduler\HpcJobTemplate\HpcJobTemplateAcl.xml" -IsFolder:$false;
+		Get-HpcJobTemplate | Get-HpcJobTemplateAcl -WV:+Global:WarningForCmdlet | Export-CliXml -Path "$Path\Scheduler\HpcJobTemplate\HpcJobTemplateAcl.xml";
+		PostExportItem;
+		
+		PreExportItem -Name:"scheduler environment variables" -Target:"$Path\Scheduler\SchedulerEnvVar.xml" -IsFolder:$false;
+		($envVars = Get-HpcClusterProperty -Environment) | ? { $bypassSchdEnvVar -inotcontains $_.Name } | Export-Clixml -Path "$Path\Scheduler\SchedulerEnvVar.xml";
+		WriteWarning "Environment variables CCP_CLUSTER_NAME, CCP_MPI_NETMASK, CCP_SERVICEREGISTRATION_PATH and WCF_NETWORKPREFIX are not exported because it's very likely they don't work with another cluster when being imported later.";
+		PostExportItem;
+
+		PreExportItem -Name:"scheduler configurations" -Target:"$Path\Scheduler\SchedulerCfg.xml" -IsFolder:$false;
+		$schdCfg = Get-HpcClusterProperty -WV:+Global:WarningForCmdlet | ? { ($envVars|%{$_.Name}) -inotcontains $_.Name -and $diagCfgNames -inotcontains $_.Name -and $mgmtCfgNames -inotcontains $_.Name -and $rptCfgNames -inotcontains $_.Name -and $bypassCfg -inotcontains $_.Name};
+		$schdCfg | Export-Clixml -Path "$Path\Scheduler\SchedulerCfg.xml";
+		if($Configuration -icontains "Management") { $Global:WarningForCmdlet = $null; }
+		PostExportItem;
+
+		$schdCfg | % {
+			if( $redirectToExportFolderCfg -icontains $_.Name -and !([string]::IsNullOrEmpty($_.Value)) ) {
+				$fileName = [System.IO.Path]::GetFileName($_.Value);
+				PreExportItem -Name:$_.Name -Target:"$Path\Scheduler\$($_.Name)\$fileName" -IsFolder:$false;
+				if (!(Test-Path "$Path\Scheduler\$($_.Name)")) { New-Item -Path "$Path\Scheduler\$($_.Name)" -ItemType directory | out-null; }
+				Copy-Item $_.Value "$Path\Scheduler\$($_.Name)\$fileName";
+				PostExportItem;
+				WriteWarning "Only one assembly file is exported for $($_.Name) If it has dependencies on other assemblies, you need to export them manually.";
+			}
+		}
+	}
+
+	if($Configuration -icontains "SOA") {
+		PreExportItem -Name:"SOA service registration files" -Target:"$Path\SOA\Registration" -IsFolder:$true;
+		$svcRegFileSource = (Get-HpcClusterRegistry -PropertyName ServiceRegistrationShare).Value
+		if( $svcRegFileSource.IndexOf(';') -ne -1) {
+			WriteWarning "When the ServiceRegistrationShare cluster property value contains multiple paths seperated by ';', only the first path is exported.";
+			$svcRegFileSource = $svcRegFileSource.SubString(0, $svcRegFileSource.IndexOf(';'));
+		}
+
+		Copy-Item "$svcRegFileSource\*" "$Path\SOA\Registration" -Recurse;
+		WriteWarning "For SOA services, only registration files are exported. You need to export service assemblies manually.";
+		PostExportItem;
+	}
+	
+	WriteWarning ("Some configurations are not exported due to either security reason or it's not supported to import them later: "+ [string]::join(", ", $bypassCfg) + ".");
+	WriteWarning "Some exported configurations may refer to the cluster name or UNC path. You need to review and change appropriately before importing them to another cluster later.";
+	$Global:Xml += '</ExportedConfigurations>';
+	$Global:Xml | Out-File "$Path\MetaData.xml" -Encoding:utf8;
+	WriteOutput "Exporting is completed. $Global:SuccessCount configurations are exported successfully. $Global:FailureCount configurations fail to be exported.";
+	if ( $error ) { $error >> "$Path\ExportError.log"; }
+	if ( $Global:Warning ) { $Global:Warning > "$Path\ExportWarning.log"; }
+	$Global:Verbose > "$Path\ExportVerbose.log";
---
+++ Scripts/Import-HpcConfiguration.ps1
@@ -0,0 +1,218 @@
+﻿<#
+
+.SYNOPSIS
+Import Windows HPC Cluster Configuration Settings.
+
+.DESCRIPTION
+This script allows a user to import Windows HPC cluster configuration settings.
+
+.PARAMETER Path
+The import source path.
+
+.PARAMETER Configuration
+The name of the configuration settings to import. Valid values: "Diagnostics","Management","Reporting","Scheduler","Soa" or none for all configurations.
+
+.PARAMETER Force
+Import the specified configuration settings without prompting for confirmation.
+
+.EXAMPLE
+Import-HpcConfiguration.ps1
+
+.EXAMPLE
+Import-HpcConfiguration.ps1 -Path:c:\export\20110520_132914 -Configuration:Scheduler
+
+.EXAMPLE
+Import-HpcConfiguration.ps1 -Path:c:\export\20110520_132914 -Configuration:Scheduler,Soa
+
+.NOTES
+
+.LINK
+
+#>
+
+Param(
+    [ValidateNotNullOrEmpty()]
+    [string] $Path,
+
+    [ValidateSet("Diagnostics","Management","Reporting","Scheduler","Soa")]
+    [string[]] $Configuration, 
+
+    [switch] $Force
+)
+
+	function PreImportItem ([string] $Name, [string] $Target) {
+		$Global:OldErrorCount = $Global:Error.Count;
+		$Global:WarningForCmdlet = $null;
+		WriteOutput ("Importing $Name from $Target.");
+	}
+	
+	function PostImportItem {
+		if( $Global:Error.Count -gt $Global:OldErrorCount ) { $Global:FailureCount++; } else { $Global:SuccessCount++; }
+		$Global:WarningForCmdlet | ? { $_ -ne $null } | % {
+			$Global:Warning += "WARNING: " + $_.Message + [environment]::NewLine + [environment]::NewLine;
+			$Global:Verbose += "WARNING: " + $_.Message + [environment]::NewLine + [environment]::NewLine;
+		}
+	}
+
+	function ImportClusterProperty ([string] $Name, [string] $Value) {
+		if( $bypassCfg -icontains $Name -or ($bypassCfgIfEmpty -icontains $Name -and [string]::IsNullOrEmpty($Value)) ) { return; }
+		if( ($Value -eq 'True') -or ($Value -eq 'False')) { "Set-HpcClusterProperty -$($Name):$"+ $Value | iex; } else { "Set-HpcClusterProperty -$($Name):'$($Value)' -WV:+Global:WarningForCmdlet" |iex; }
+	}
+
+	function ImportClusterRegistry ([string] $Name, [string] $Value) {
+		if( $bypassRegistry -icontains $Name -or [string]::IsNullOrEmpty($Value) ) { return; }
+		"Set-HpcClusterRegistry -PropertyName $Name -PropertyValue '$Value' -WV:+Global:WarningForCmdlet" | iex;
+	}
+	
+	function WriteWarning( [string] $warning) {
+		Write-Warning $warning;
+		$Global:Warning += "WARNING: $warning" + [environment]::NewLine + [environment]::NewLine;
+		$Global:Verbose += "WARNING: $warning" + [environment]::NewLine + [environment]::NewLine;
+	}
+	
+	function WriteOutput( [string] $output) {
+		Write-Output $output;
+		$Global:Verbose += $output + [environment]::NewLine + [environment]::NewLine;
+	}
+
+	$error.Clear();
+
+	$errorMsgNonElevatedProcess = "This function needs to be run with elevated PowerShell.";
+	$currentPrincipal = New-Object Security.Principal.WindowsPrincipal( [Security.Principal.WindowsIdentity]::GetCurrent() );
+	if(!$currentPrincipal.IsInRole( [Security.Principal.WindowsBuiltInRole]::Administrator )) { Write-Error $errorMsgNonElevatedProcess; return; }
+
+	$errorMsgCannotOnlyRunOnHeadNode = "This function needs to be run on head node.";
+	if( !(Test-Path HKLM:\Software\Microsoft\HPC) -or (Get-ItemProperty -Path:HKLM:\Software\Microsoft\HPC).InstalledRole -inotcontains "HN") { Write-Error $errorMsgCannotOnlyRunOnHeadNode; return; }
+
+	if( !(Get-PsSnapIn Microsoft.HPC -EA:0) ) { Add-PsSnapIn Microsoft.HPC; }
+	$error.Clear();
+
+	$Path = [System.IO.Path]::GetFullPath($Path);
+
+	WriteOutput("Setting importing source folder to $Path.");
+	$errorMsgCannotFindSourceFolder = "Cannot find importing source folder $Path.";
+	if( !(Test-Path $Path)) { Write-Error $errorMsgCannotFindSourceFolder; return; }
+
+	if($Configuration -eq $null) { $Configuration = "Diagnostics","Management","Reporting","Scheduler","Soa"; }
+
+	[xml]$metadata = get-content $Path\MetaData.xml;
+	$exportedVersion = $metadata.ExportedConfigurations.HpcVersion;
+	$importedVersion = (Get-HpcClusterOverview).Version;
+
+	if($error) { return; }
+	if($exportedVersion -ne $importedVersion) {
+		WriteWarning "The configurations was exported from cluster with version $exportedVersion. They may be not fully compatible with the importing cluster with version $importedVersion.";
+	}
+
+	$Global:SuccessCount = $Global:FailureCount = 0;
+	$Global:Warning = $Global:Verbose = "";
+	$redirectToExportFolderCfg = "ActivationFilterProgram", "SubmissionFilterProgram";
+	$bypassCfg = "ActivationFilterProgram","AdFailurePercentageEntry","EmailCredential","EventListenerClosePercentage","InstallCredential","SchedulerOnAzure","SpoolDir","SqlReadFailurePercentageEntry","SqlTransactionFailurePercentage","SubmissionFilterProgram","ReportingDbSize","ClusterId", "AzureBurstFailurePercentageEntry", "ScanAzureBatchTaskWithoutFilterInterval", "PriorityBiasLevel", "CustomNodeSorterPath","CustomNodeSorterTimeout";
+	$bypassCfgIfEmpty = "NodeNamingSeries","JobCleanUpDayOfWeek","";
+	$bypassSchdEnvVar = "CCP_CLUSTER_NAME","CCP_MPI_NETMASK","CCP_SERVICEREGISTRATION_PATH","WCF_NETWORKPREFIX";
+    $bypassRegistry = "ClusterName","ClusterId","SQM","RuntimeDataShare","InstallShare","SpoolDirShare","DiagnosticsShare","ServiceRegistrationShare","NetworkTopology","ReportingDbConnectionString","DiagnosticsDbConnectionString","MonitoringDbConnectionString","SchedulerDbConnectionString","ManagementDbConnectionString","ManagementDbServerName","SchedulerDbServerName","ReportingDbServerName","DiagnosticsDbServerName","MonitoringDbServerName","SSLThumbprint","VNet","Subnet","Location","ResourceGroup";
+
+	if($Configuration -icontains "Diagnostics") {
+		PreImportItem -Name:"diagnostics configurations" -Target:"$Path\Diagnostics\DiagnosticsCfg.xml";
+		Import-Clixml -Path "$Path\Diagnostics\DiagnosticsCfg.xml" | %{ ImportClusterProperty -Name:"$($_.Name)" -Value:"$($_.Value)"; }
+		PostImportItem;
+	}
+
+	if($Configuration -icontains "Management") {
+		PreImportItem -Name:"drivers" -Target:"$Path\Management\HpcDriver";
+		dir "$Path\Management\HpcDriver" -Recurse | ? { $_.Name.EndsWith(".inf") } | % { Add-HpcDriver -Path:"$($_.FullName)" -WV:+Global:WarningForCmdlet } | out-null;
+		PostImportItem;
+
+		PreImportItem -Name:"images" -Target:"$Path\Management\HpcImage";
+		dir "$Path\Management\HpcImage\*.wim" | % { Add-HpcImage -Path:"$($_.FullName)" -WV:+Global:WarningForCmdlet } | out-null;
+		PostImportItem;
+
+		PreImportItem -Name:"users" -Target:"$Path\Management\HpcUser.xml";
+		Import-Clixml -Path "$Path\Management\HpcUser.xml" | ? { !($_.Name.EndsWith("$")) } | %{ Add-HpcMember -Name:"$($_.Name)" -Role:"$($_.Role.Value)" -WV:+Global:WarningForCmdlet } | out-null;
+		WriteWarning "Machine accounts are not imported as HPC users for security reason. You can add them manually if needed.";
+		PostImportItem;
+
+		PreImportItem -Name:"node groups" -Target:"$Path\Management\HpcNodeGroup.xml";
+		#Fix for 21688; We modify $Global:OldErrorCount to ignore the error introduced by Get-HpcGroup. The error from this cmdlet, standing for node group not exists, is expected and should not be considered in the overall failure count.
+		Import-Clixml -Path "$Path\Management\HpcNodeGroup.xml" | %{$errorBefore = $Global:Error.Count; if( !(Get-HpcGroup -Name:"$($_.Name)" -EA:0) ) { $Global:OldErrorCount += ($Global:Error.Count - $errorBefore); New-HpcGroup -Name:"$($_.Name)" -Description:"$($_.Description)" -WV:+Global:WarningForCmdlet | out-null } }
+		PostImportItem;
+
+		PreImportItem -Name:"node templates" -Target:"$Path\Management\HpcNodeTemplate";
+		dir "$Path\Management\HpcNodeTemplate\*.xml" | %{ Import-HpcNodeTemplate -Path:$_.FullName -Upgrade -Force:$(if($Force){$true}else{$false}) -WV:+Global:WarningForCmdlet | out-null; }
+		PostImportItem;
+
+		PreImportItem -Name:"management configurations" -Target:"$Path\Management\ManagementCfg.xml";
+		Import-Clixml -Path "$Path\Management\ManagementCfg.xml" | % { ImportClusterProperty -Name:"$($_.Name)" -Value:"$($_.Value)"; }
+		PostImportItem;
+
+		PreImportItem -Name:"hpc metrics" -Target:"$Path\Management\Metrics.xml";
+		Import-HpcMetric -Path "$Path\Management\Metrics.xml";
+		PostImportItem;
+
+		PreImportItem -Name:"hpc nodes" -Target:"$Path\Management\HpcNode.xml";
+		Import-HpcNodeXml -Path "$Path\Management\HpcNode.xml";
+		PostImportItem;
+	}
+
+	if($Configuration -icontains "Reporting") {
+		PreImportItem -Name:"reporting configurations" -Target:"$Path\Reporting\ReportingCfg.xml";
+		Import-Clixml -Path "$Path\Reporting\ReportingCfg.xml" | %{ ImportClusterProperty -Name:"$($_.Name)" -Value:"$($_.Value)"; }
+		PostImportItem;
+	}
+
+	if($Configuration -icontains "Scheduler") {
+		PreImportItem -Name:"job templates" -Target:"$Path\Scheduler\HpcJobTemplate";
+		dir "$Path\Scheduler\HpcJobTemplate\*.xml" | ?{$_.Name -ine "HpcJobTemplateAcl.xml"} | %{ Import-HpcJobTemplate -Name:$_.Name.SubString(0, $_.Name.Length-4) -Path:$_.FullName -Force:$(if($Force){$true}else{$false}) -WV:+Global:WarningForCmdlet | out-null; }
+		PostImportItem;
+
+		PreImportItem -Name:"job template ACL" -Target:"$Path\Scheduler\HpcJobTemplate\HpcJobTemplateAcl.xml";
+		Import-Clixml -Path "$Path\Scheduler\HpcJobTemplate\HpcJobTemplateAcl.xml" | %{
+			$sddl = New-Object System.Security.AccessControl.CommonSecurityDescriptor $false, $false, $_.Sddl;
+			Set-HpcJobTemplateAcl -Name:$_.JobTemplateName -Acl:(New-Object Microsoft.ComputeCluster.CCPPSH.HpcJobTemplateSecurityDescriptor $_.JobTemplateName,$sddl,$env:CCP_SCHEDULER ) -WV:+Global:WarningForCmdlet;
+		}
+		PostImportItem;
+
+		PreImportItem -Name:"scheduler environment variables" -Target:"$Path\Scheduler\SchedulerEnvVar.xml";
+		Import-Clixml -Path "$Path\Scheduler\SchedulerEnvVar.xml" | ?{ $bypassSchdEnvVar -inotcontains $_.Name } | % { Set-HpcClusterProperty -Environment:"$($_.Name)=$($_.Value)" -WV:+Global:WarningForCmdlet }
+		WriteWarning "Environment variables CCP_CLUSTER_NAME, CCP_MPI_NETMASK, CCP_SERVICEREGISTRATION_PATH and WCF_NETWORKPREFIX are not imported. You may need to review and configure manually.";
+		PostImportItem;
+
+		PreImportItem -Name:"scheduler configurations" -Target:"$Path\Scheduler\SchedulerCfg.xml";
+		Import-Clixml -Path "$Path\Scheduler\SchedulerCfg.xml" | % {
+			ImportClusterProperty -Name:"$($_.Name)" -Value:"$($_.Value)";
+			if( $redirectToExportFolderCfg -icontains $_.Name -and !([string]::IsNullOrEmpty($_.Value)) ) {
+				$fileName = [System.IO.Path]::GetFileName($_.Value);
+				if( Test-Path "$Path\Scheduler\$($_.Name)\$fileName" ) {
+					"Set-HpcClusterProperty -$($_.Name):'$Path\Scheduler\$($_.Name)\$fileName' -WV:+Global:WarningForCmdlet" | iex;
+					WriteWarning "Only one assembly file is imported for $($_.Name) If it has dependencies on other assemblies, you need to copy them manually.";
+				}
+			}
+		}
+		PostImportItem;
+	}
+
+	if($Configuration -icontains "SOA") {
+		PreImportItem -Name:"SOA service registration files" -Target:"$Path\SOA\Registration";
+        $svcRegFileSource = (Get-HpcClusterRegistry -PropertyName ServiceRegistrationShare).Value
+		if( $svcRegFileSource.IndexOf(';') -ne -1) {
+			WriteWarning "When the ServiceRegistrationShare cluster property value contains multiple paths seperated by ';', only the first path is imported.";
+			$svcRegFileSource = $svcRegFileSource.SubString(0, $svcRegFileSource.IndexOf(';'));
+		}
+		Copy-Item "$Path\SOA\Registration\*" "$svcRegFileSource" -Recurse -Force:$(if($Force){$true}else{$false});
+		WriteWarning "For SOA services, only registration files are imported. You need to import service assemblies manually.";
+		PostImportItem;
+	}
+    
+    PreImportItem -Name:"Cluster registry" -Target:"$Path\ClusterRegistry.xml";
+    Import-Clixml -Path "$Path\ClusterRegistry.xml" | ?{$bypassRegistry -inotcontains $_.Name} | %{ImportClusterRegistry -Name $_.Name -Value $_.Value}
+    PostImportItem
+	
+	WriteWarning ("Some configurations are not imported due to either security reason or it's not supported to import them: "+ [string]::join(", ", $bypassCfg) + ".");
+	WriteWarning "Some imported configurations may refer to the original cluster name or UNC path. You need to review and change appropriately.";
+	WriteWarning "This script didn't import any credentials, including: installation credentials, email credentials, diagnostics credentials, SOA session credentials, credentials configured in node template and Windows Azure storage keys."
+	WriteWarning "Please provide these credentials after importing, if any of these credentials are in use."
+	WriteOutput "Importing is completed. $Global:SuccessCount configurations are imported successfully. $Global:FailureCount configurations fail to be imported.";
+	$now = [System.DateTime]::Now.ToString('yyyyMMdd_HHmmss');
+	if ( $error ) { $error > "$Path\ImportError_$now.log"; }
+	if ( $Global:Warning ) { $Global:Warning > "$Path\ImportWarning_$now.log"; }
+	$Global:Verbose > "$Path\ImportVerbose_$now.log";
