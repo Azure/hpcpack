43971c7a3a905a8cce457d14c1fd319ff9c5a368
tianyiliu <tianyiliu@microsoft.com>
2024-05-10 11:33:31 +0800

    Update Deploy-Kubernetes.sh

--- Scripts/Deploy-Kubernetes.sh
+++ Scripts/Deploy-Kubernetes.sh
@@ -1,31 +1,36 @@
 #!/bin/bash
 
-sudo apt update
-echo "Installing Python 3.10 and Pip"
-sudo apt install software-properties-common -y
-sudo add-apt-repository ppa:deadsnakes/ppa -y
-sudo apt update
-sudo apt install python3.10 python3.10-venv python3.10-dev -y
-sudo rm /usr/bin/python3
-sudo ln -s python3.10 /usr/bin/python3
-sudo ln -s /usr/lib/python3/dist-packages/apt_inst.cpython-38-x86_64-linux-gnu.so /usr/lib/python3/dist-packages/apt_inst.so
-sudo ln -s /usr/lib/python3/dist-packages/apt_pkg.cpython-38-x86_64-linux-gnu.so /usr/lib/python3/dist-packages/apt_pkg.so
-curl https://bootstrap.pypa.io/get-pip.py | sudo python3
+install_python() {
+    sudo apt update
+    echo "Installing Python 3.10 and Pip"
+    sudo apt install software-properties-common -y
+    sudo add-apt-repository ppa:deadsnakes/ppa -y
+    sudo apt update
+    sudo apt install python3.10 python3.10-venv python3.10-dev -y
+    sudo rm /usr/bin/python3
+    sudo ln -s python3.10 /usr/bin/python3
+    sudo ln -s /usr/lib/python3/dist-packages/apt_inst.cpython-38-x86_64-linux-gnu.so /usr/lib/python3/dist-packages/apt_inst.so
+    sudo ln -s /usr/lib/python3/dist-packages/apt_pkg.cpython-38-x86_64-linux-gnu.so /usr/lib/python3/dist-packages/apt_pkg.so
+    curl https://bootstrap.pypa.io/get-pip.py | sudo python3
 
-echo "Testing python3 and pip3"
-python3 -V
-pip3 -V
-echo "End of Installing Python 3.10 and Pip"
-# end of Installing Python 3.10 and Pip
-
-echo "Generating ssh key and copying to all nodes"
-sudo apt install sshpass -y
-ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
+    echo "Testing python3 and pip3"
+    python3 -V
+    pip3 -V
+    echo "End of Installing Python 3.10 and Pip"
+    # end of Installing Python 3.10 and Pip
+}
 
 # Function to display usage information
 usage() {
     echo "Usage: $0 [-p password] [ip1 ip2 ...]" 1>&2
     exit 1
+}
+
+# Function to resolve hostname to IP address
+resolve_hostname() {
+    hostname=$1
+    ip=$(dig +short $hostname)
+    echo $ip
 }
 
 # Parse command line options
@@ -58,35 +63,51 @@ if [ -z "$password" ]; then
     echo
 fi
 
+# install_python
+
 IPS=()
 
 # Loop through each IP address and copy SSH key
-for ip in "$@"; do
-    IPS+=$(dig +short $ip)
-    echo "Copying SSH key to $ip..."
-    sshpass -p "$password" ssh-copy-id -o StrictHostKeyChecking=no "$ip"
-    if [ $? -eq 0 ]; then
-        echo "SSH key copied successfully to $ip."
-    else
-        echo "Failed to copy SSH key to $ip. Please check the password or connectivity."
-    fi
+for hostname in "$@"; do
+    # Resolve the hostname to IP address and append to the array
+    ip=$(resolve_hostname $hostname)
+    IPS+=($ip)
 done
 
-echo "Installing and disabling firewalld"
-sudo apt install firewalld -y
-sudo systemctl disable --now firewalld
+# echo "Generating ssh key and copying to all nodes"
+# sudo apt install sshpass -y
+# ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
+# for ip in "${IPS[@]}"; do
+#     echo "Copying SSH key to $ip..."
+#     # Copy SSH key to the IP address
+#     sshpass -p "$password" ssh-copy-id -o StrictHostKeyChecking=no "$ip"
+#     if [ $? -eq 0 ]; then
+#         echo "SSH key copied successfully to $ip."
+#     else
+#         echo "Failed to copy SSH key to $ip. Please check the password or connectivity."
+#     fi
+# done
+
+# echo "------------------------------------------"
+# echo "Installing and disabling firewalld"
+# sudo apt install firewalld -y
+# sudo systemctl disable --now firewalld
 
 
-echo "Installing and configuring Kubernetes via kubespray"
-git clone https://github.com/kubernetes-sigs/kubespray
+# echo "Installing and configuring Kubernetes via kubespray"
+# git clone https://github.com/kubernetes-sigs/kubespray
 cd kubespray
 # We may customize the version of kubespray here
 git checkout release-2.24
 sudo pip3 install -r requirements.txt
+echo "------------------------------------------"
 cp -rfp inventory/sample inventory/mycluster
 CONFIG_FILE=inventory/mycluster/hosts.yaml python3 contrib/inventory_builder/inventory.py ${IPS[@]}
+echo "------------------------------------------"
 echo "yes" | ansible-playbook -i inventory/mycluster/hosts.yaml  --become --become-user=root reset.yml
+echo "------------------------------------------"
 ansible-playbook -i inventory/mycluster/hosts.yaml  --become --become-user=root cluster.yml
+echo "------------------------------------------"
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
