58fa1afd2b15491fdbb6358f454649c9e3e920ad
tianyiliu <tianyiliu@microsoft.com>
2024-06-19 17:43:26 +0800

    Update Program.cs

--- code/KubernetesAPP/KubernetesAPP/Program.cs
+++ code/KubernetesAPP/KubernetesAPP/Program.cs
@@ -6,20 +6,22 @@ namespace KubernetesAPP
     {
         private static void Main(string[] args)
         {
-            var config = KubernetesClientConfiguration.BuildConfigFromConfigFile("~/.kube/config");
-            IKubernetes client = new Kubernetes(config);
-            Console.WriteLine("Starting Request!");
+            //var config = KubernetesClientConfiguration.BuildConfigFromConfigFile("~/.kube/config");
+            //IKubernetes client = new Kubernetes(config);
+            //Console.WriteLine("Starting Request!");
 
-            var list = client.CoreV1.ListNamespacedPod("default");
-            foreach (var item in list.Items)
-            {
-                Console.WriteLine(item.Metadata.Name);
-            }
+            //var list = client.CoreV1.ListNamespacedPod("default");
+            //foreach (var item in list.Items)
+            //{
+            //    Console.WriteLine(item.Metadata.Name);
+            //}
 
-            if (list.Items.Count == 0)
-            {
-                Console.WriteLine("Empty!");
-            }
+            //if (list.Items.Count == 0)
+            //{
+            //    Console.WriteLine("Empty!");
+            //}
+            var fi = new FileInfo("~/.kube/config");
+            Console.WriteLine(fi.DirectoryName);
         }
     }
 }
