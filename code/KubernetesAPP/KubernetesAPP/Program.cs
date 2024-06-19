1994a5f70ce9fe4f3eb95a979b7062a3ef384549
tianyiliu <tianyiliu@microsoft.com>
2024-06-19 16:24:54 +0800

    create project

---
+++ code/KubernetesAPP/KubernetesAPP/Program.cs
@@ -0,0 +1,25 @@
+﻿using k8s;
+
+namespace KubernetesAPP
+{
+    internal class PodList
+    {
+        private static void Main(string[] args)
+        {
+            var config = KubernetesClientConfiguration.BuildConfigFromConfigFile("~/.kube/config");
+            IKubernetes client = new Kubernetes(config);
+            Console.WriteLine("Starting Request!");
+
+            var list = client.CoreV1.ListNamespacedPod("default");
+            foreach (var item in list.Items)
+            {
+                Console.WriteLine(item.Metadata.Name);
+            }
+
+            if (list.Items.Count == 0)
+            {
+                Console.WriteLine("Empty!");
+            }
+        }
+    }
+}
