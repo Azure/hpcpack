14387b9f43787348b90a44a62bedfc50573a73b9
tianyiliu <tianyiliu@microsoft.com>
2024-06-19 18:58:29 +0800

    Update Program.cs

--- code/KubernetesAPP/KubernetesAPP/Program.cs
+++ code/KubernetesAPP/KubernetesAPP/Program.cs
@@ -6,22 +6,23 @@ namespace KubernetesAPP
     {
         private static void Main(string[] args)
         {
-            //var config = KubernetesClientConfiguration.BuildConfigFromConfigFile("~/.kube/config");
-            //IKubernetes client = new Kubernetes(config);
-            //Console.WriteLine("Starting Request!");
+            string? homeDirectory = Environment.GetEnvironmentVariable("HOME");
+            homeDirectory = homeDirectory ?? "/home/hpcadmin";
+            var config = KubernetesClientConfiguration.BuildConfigFromConfigFile($"{ homeDirectory }/config");
+            IKubernetes client = new Kubernetes(config);
+            Console.WriteLine("Starting Request!");
 
-            //var list = client.CoreV1.ListNamespacedPod("default");
-            //foreach (var item in list.Items)
-            //{
-            //    Console.WriteLine(item.Metadata.Name);
-            //}
+            var list = client.CoreV1.ListNamespacedPod("default");
+            foreach (var item in list.Items)
+            {
+                Console.WriteLine(item.Metadata.Name);
+            }
 
-            //if (list.Items.Count == 0)
-            //{
-            //    Console.WriteLine("Empty!");
-            //}
-            string homeDirectory = Environment.GetEnvironmentVariable("HOME");
-            Console.WriteLine(homeDirectory);
+            if (list.Items.Count == 0)
+            {
+                Console.WriteLine("Empty!");
+            }
+
         }
     }
 }
