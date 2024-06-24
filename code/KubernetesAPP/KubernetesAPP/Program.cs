05d79fa4dc6d8fc15fe824da8b8f7f958b1bfb65
tianyiliu <tianyiliu@microsoft.com>
2024-06-24 16:23:58 +0800

    Update Program.cs

--- code/KubernetesAPP/KubernetesAPP/Program.cs
+++ code/KubernetesAPP/KubernetesAPP/Program.cs
@@ -13,26 +13,61 @@ namespace KubernetesAPP
             IKubernetes client = new Kubernetes(config);
             Console.WriteLine("Starting Request!");
 
-            client.CoreV1.CreateNamespacedPod(new V1Pod
+            //client.CoreV1.CreateNamespacedPod(new V1Pod
+            //{
+            //    Metadata = new V1ObjectMeta
+            //    {
+            //        Name = "test-pod"
+            //    },
+            //    Spec = new V1PodSpec
+            //    {
+            //        Containers = new List<V1Container>
+            //        {
+            //            new V1Container
+            //            {
+            //                Name = "test-container",
+            //                Image = "busybox",
+            //                Command = new [] { "sleep", "5" }
+            //            }
+            //        }
+            //    }
+            //}, "default");
+
+            client.BatchV1.CreateNamespacedJob(new V1Job
             {
+                ApiVersion = "batch/v1",
+                Kind = "Job",
                 Metadata = new V1ObjectMeta
                 {
-                    Name = "test-pod"
+                    Name = "busybox-job"
                 },
-                Spec = new V1PodSpec
+                Spec = new V1JobSpec
                 {
-                    Containers = new List<V1Container>
+                    TtlSecondsAfterFinished = 10,
+                    Template = new V1PodTemplateSpec
                     {
-                        new V1Container
+                        Metadata = new V1ObjectMeta
                         {
-                            Name = "test-container",
-                            Image = "busybox",
-                            Command = new List<string> { "sleep", "5" }
+                            Name = "busybox-pod"
+                        },
+                        Spec = new V1PodSpec
+                        {
+                            RestartPolicy = "Never",
+                            Containers = new[]
+                            {
+                                new V1Container
+                                {
+                                    Name = "busybox-container",
+                                    Image = "busybox",
+                                    Command = new [] { "sleep", "5" }
+                                }
+                            }
                         }
                     }
                 }
             }, "default");
 
+            Console.WriteLine("Request Completed!");
         }
     }
 }
