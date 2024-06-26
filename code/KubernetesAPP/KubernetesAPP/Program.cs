ec356c73997127b361cb8a95d4208ba4ff8daafc
tianyiliu <tianyiliu@microsoft.com>
2024-06-26 17:56:50 +0800

    Update Program.cs

--- code/KubernetesAPP/KubernetesAPP/Program.cs
+++ code/KubernetesAPP/KubernetesAPP/Program.cs
@@ -73,11 +73,14 @@ namespace KubernetesAPP
             //var podlistResp = client.CoreV1.ListNamespacedPodAsync("default", watch: true);
             var podlistResp = client.CoreV1.ListNamespacedPodWithHttpMessagesAsync("default", watch: true);
             // C# 8 required https://docs.microsoft.com/en-us/archive/msdn-magazine/2019/november/csharp-iterating-with-async-enumerables-in-csharp-8
+            
+            
             await foreach (var (type, item) in podlistResp.WatchAsync<V1Pod, V1PodList>())
             {
                 Console.WriteLine("==on watch event==");
                 Console.WriteLine(type);
                 Console.WriteLine(item.Metadata.Name);
+                Console.WriteLine(item.Status.Phase);
                 Console.WriteLine("==on watch event==");
             }
         }
