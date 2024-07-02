f60fe44193238c795a45f5e6829fec8739b82090
tianyiliu <tianyiliu@microsoft.com>
2024-07-02 17:26:18 +0800

    Update Program.cs

--- code/KubernetesAPP/KubernetesAPP/Program.cs
+++ code/KubernetesAPP/KubernetesAPP/Program.cs
@@ -15,27 +15,33 @@ namespace KubernetesAPP
             //    return;
             //}
             //var (podName, containerName, imageName, namespaceName, command, arguments) = Util.ProcessArgs(args);
+            var deploymentName = "busybox-deployment";
+            var containerName = "busybox";
+            var imageName = "busybox";
+            var namespaceName = "default";
+            var command = new[] { "sleep", "3600" };
+            var arguments = new[] { "" };
+            var nodeList = new[] {"node3", "node4"};
+            Console.WriteLine($"deployment Name: {deploymentName}");
+            Console.WriteLine($"Container Name: {containerName}");
+            Console.WriteLine($"Image Name: {imageName}");
+            Console.WriteLine($"Namespace Name: {namespaceName}");
+            Console.WriteLine("----");
 
-            //Console.WriteLine($"Pod Name: {podName}");
-            //Console.WriteLine($"Container Name: {containerName}");
-            //Console.WriteLine($"Image Name: {imageName}");
-            //Console.WriteLine($"Namespace Name: {namespaceName}");
-            //Console.WriteLine("----");
+            foreach (var item in command)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("----");
 
-            //foreach (var item in command)
-            //{
-            //   Console.WriteLine(item);
-            //}
-            //Console.WriteLine("----");
-
-            //foreach (var item in arguments)
-            //{
-            //    Console.WriteLine(item);
-            //}
-            //Console.WriteLine("----");
+            foreach (var item in arguments)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("----");
 
             string? homeDirectory = Environment.GetEnvironmentVariable("HOME");
-            homeDirectory = homeDirectory ?? "/home/hpcadmin";
+            homeDirectory ??= "/home/hpcadmin";
             var config = KubernetesClientConfiguration.BuildConfigFromConfigFile($"{homeDirectory}/.kube/config");
             IKubernetes client = new Kubernetes(config);
 
@@ -48,17 +54,12 @@ namespace KubernetesAPP
                 e.Cancel = true; // Prevent the process from terminating immediately
             };
 
-            var v1 = Environment.GetEnvironmentVariable("key1");
-            var v2 = Environment.GetEnvironmentVariable("key2");
-            Console.WriteLine($"key1: {v1}");
-            Console.WriteLine($"key2: {v2}");
-
-            //var pod = await CreatePod(client, podName, containerName, imageName, namespaceName, command, arguments);
-            //Console.WriteLine($"Pod {podName} created.");
+            var pod = await CreateDeployment(client, deploymentName, containerName, imageName, namespaceName, command, arguments, nodeList);
+            Console.WriteLine($"Deployment {deploymentName} created.");
 
             //var podWatcher = client.CoreV1.ListNamespacedPodWithHttpMessagesAsync(
             //    "default",
-            //    fieldSelector: $"metadata.name={podName}",
+            //    fieldSelector: $"metadata.name={deploymentName}",
             //    watch: true);
 
             //await foreach (var (type, item) in podWatcher.WatchAsync<V1Pod, V1PodList>(
@@ -75,55 +76,95 @@ namespace KubernetesAPP
 
             //    if (item.Status.Phase == RUNNINGSTATUS)
             //    {
-            //        Console.WriteLine($"Pod {podName} is running. Exit monitoring.");
+            //        Console.WriteLine($"Pod {deploymentName} is running. Exit monitoring.");
             //        break;
             //    }
             //}
         }
 
-        public static async Task<V1Pod?> CreatePod(IKubernetes client, string podName, string containerName, string imageName, string namespaceName, string[] command, string[] arguments)
+        public static async Task<V1Deployment?> CreateDeployment(IKubernetes client, string deploymentName, string containerName, string imageName, 
+            string namespaceName, string[] command, string[] arguments, string[] nodeList)
         {
-            var pod = new V1Pod
+            var deployment = new V1Deployment
             {
+                ApiVersion = "apps/v1",
+                Kind = "Deployment",
                 Metadata = new V1ObjectMeta
                 {
-                    Name = podName,
-                    NamespaceProperty = namespaceName
+                    Name = deploymentName
                 },
-                Spec = new V1PodSpec
+                Spec = new V1DeploymentSpec
                 {
-                    Containers =
-                    [
-                        new V1Container
+                    Replicas = nodeList.Length,
+                    Selector = new V1LabelSelector
+                    {
+                        MatchLabels = new System.Collections.Generic.Dictionary<string, string>
                         {
-                            Name = containerName,
-                            Image = imageName,
-                            Command = command,
-                            Args = arguments
+                            { "app", "busybox" }
                         }
-                    ],
-                    RestartPolicy = "Always"
+                    },
+                    Template = new V1PodTemplateSpec
+                    {
+                        Metadata = new V1ObjectMeta
+                        {
+                            Labels = new System.Collections.Generic.Dictionary<string, string>
+                            {
+                                { "app", "busybox" }
+                            }
+                        },
+                        Spec = new V1PodSpec
+                        {
+                            Containers =
+                            [
+                                new()
+                                {
+                                    Name = containerName,
+                                    Image = imageName,
+                                    Command = command,
+                                    Args = arguments
+                                }
+                            ],
+                            Affinity = new V1Affinity
+                            {
+                                NodeAffinity = new V1NodeAffinity
+                                {
+                                    RequiredDuringSchedulingIgnoredDuringExecution = new V1NodeSelector
+                                    {
+                                        NodeSelectorTerms =
+                                        [
+                                            new V1NodeSelectorTerm
+                                            {
+                                                MatchExpressions =
+                                                [
+                                                    new()
+                                                    {
+                                                        Key = "kubernetes.io/hostname",
+                                                        OperatorProperty = "In",
+                                                        Values = nodeList
+                                                    }
+                                                ]
+                                            }
+                                        ]
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
             };
 
-            V1Pod? createdPod;
+            V1Deployment? result = null;
             try
             {
-                // Create the pod in the default namespace
-                createdPod = await client.CoreV1.CreateNamespacedPodAsync(pod, "default");
+                result = await client.AppsV1.CreateNamespacedDeploymentAsync(deployment, namespaceName);
+                Console.WriteLine($"Deployment '{deploymentName}' created successfully.");
             }
-            catch (HttpOperationException e)
+            catch (Exception ex)
             {
-                Console.WriteLine($"Exception in creating pod: {e.Message}");
-                return null;
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"Unknown exception in creating pod: {e.Message}");
-                return null;
+                Console.WriteLine($"Error creating deployment: {ex.Message}");
             }
 
-            return createdPod;
+            return result;
         }
     }
 }
