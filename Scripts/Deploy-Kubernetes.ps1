f450f77f2fe30460d0ca4c6743099b552c3843f7
tianyiliu <tianyiliu@microsoft.com>
2024-07-10 17:13:53 +0800

    update

---
+++ Scripts/Deploy-Kubernetes.ps1
@@ -0,0 +1,9 @@
+param (
+    [string]$password,
+    [string[]]$servers
+)
+
+$joinedServers = $servers -join ' '
+
+clusrun /nodes:$($servers[0]) "git clone https://github.com/Azure/hpcpack.git && cd hpcpack && git checkout tianyiliu/deploy-Kubernetes-script && chmod +x Scripts/Deploy-Kubernetes.sh"
+clusrun /nodes:$($servers[0]) "hpcpack/Scripts/Deploy-Kubernetes.sh -p '$password' $joinedServers"
