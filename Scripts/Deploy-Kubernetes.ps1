496f827b933247a78ce80327ac997e2166d596b7
tianyiliu <tianyiliu@microsoft.com>
2024-07-11 15:05:16 +0800

    Update Deploy-Kubernetes.ps1

--- Scripts/Deploy-Kubernetes.ps1
+++ Scripts/Deploy-Kubernetes.ps1
@@ -5,12 +5,12 @@ param (
 
 $joinedServers = $servers -join ' '
 
-# clusrun /nodes:$($servers[0]) "git clone https://github.com/Azure/hpcpack.git && cd hpcpack && git checkout tianyiliu/deploy-Kubernetes-script && chmod +x Scripts/Deploy-Kubernetes.sh"
-# clusrun /nodes:$($servers[0]) "hpcpack/Scripts/Deploy-Kubernetes.sh -p '$password' $joinedServers"
-try {
-    New-HpcGroup -Name "Kubernetes"
-} catch {
-    write-host "Group Kubernetes already exists"
-}
+clusrun /nodes:$($servers[0]) "git clone https://github.com/Azure/hpcpack.git && cd hpcpack && git checkout tianyiliu/deploy-Kubernetes-script && chmod +x Scripts/Deploy-Kubernetes.sh"
+clusrun /nodes:$($servers[0]) "hpcpack/Scripts/Deploy-Kubernetes.sh -p '$password' $joinedServers"
+# try {
+#     New-HpcGroup -Name "Kubernetes"
+# } catch {
+#     write-host "Group Kubernetes already exists"
+# }
 
 Add-HpcGroup -Name "Kubernetes" -NodeName $servers
