08244cbba73d30580b49afd168ad8fd2e9dda609
tianyiliu <tianyiliu@microsoft.com>
2024-11-18 18:43:10 +0800

    remove git checkout

--- Scripts/Deploy-Kubernetes-Kubespray.ps1
+++ Scripts/Deploy-Kubernetes-Kubespray.ps1
@@ -5,7 +5,7 @@ param (
 
 $joinedServers = $servers -join ' '
 
-clusrun /nodes:$($servers[0]) "git clone https://github.com/Azure/hpcpack.git && cd hpcpack && git checkout tianyiliu/deploy-Kubernetes-script && chmod +x Scripts/Deploy-Kubernetes.sh"
+clusrun /nodes:$($servers[0]) "git clone https://github.com/Azure/hpcpack.git && cd hpcpack && chmod +x Scripts/Deploy-Kubernetes.sh"
 clusrun /nodes:$($servers[0]) "hpcpack/Scripts/Deploy-Kubernetes.sh -p '$password' $joinedServers"
 
 New-HpcGroup -Name "Kubernetes" -ErrorAction SilentlyContinue
